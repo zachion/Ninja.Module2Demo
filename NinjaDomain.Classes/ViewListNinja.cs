c1d96a912b975bb8f751f596585b0a0415a81e1f
Zach Ioannou <zioannou77@gmail.com>
2017-05-03 18:09:07 +0300

    added the ViewListNinja class

---
+++ NinjaDomain.Classes/ViewListNinja.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NinjaDomain.Classes
+{
+    public class ViewListNinja
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool ServedInOniwaban { get; set; }
+        public string ClanName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+
+    }
+}
